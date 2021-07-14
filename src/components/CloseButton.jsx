b20ab76d2f4234ef033c0831ff93a2441711e63a
Keith <ozmaneku@gmail.com>
2021-07-14 11:48:36 -0400

    Adjusted colors

--- src/components/CloseButton.jsx
+++ src/components/CloseButton.jsx
@@ -5,6 +5,7 @@ class CloseButton extends Component {
     closeArt () {
         document.getElementById('topLayer').classList.remove('topLayer')
         document.getElementById('topLayer').style.backgroundImage = 'none'
+        document.getElementById('topLayer').style.backgroundColor = 'rgb(243,236,218)'
         document.getElementById('gridContainer').classList.remove('bottomLayer')
         document.getElementById('closeButton').classList.add('closeButton')
         document.getElementById('closeButton').classList.remove('showButton')
